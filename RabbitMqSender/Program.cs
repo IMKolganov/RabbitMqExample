83437e824ed6fdec6766b210f309fc725cbfc2f1
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2024-08-18 12:42:38 +0300

    init

---
+++ RabbitMqSender/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World! Sender.");
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+    routingKey: "hello",
+    basicProperties: null,
+    body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+
+
+string queueName = "temperatureResponseQueue";
+using var channel2 = connection.CreateModel();
+
+channel2.QueueDeclare(queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+const string message2 = "{\n  \"sensorId\": \"sensor-12345\",\n  \"location\": \"Living Room\",\n  \"timestamp\": \"2024-08-15T14:30:00Z\",\n  \"temperature\": {\n    \"value\": 22.5,\n    \"unit\": \"Celsius\"\n  },\n  \"humidity\": {\n    \"value\": 55,\n    \"unit\": \"%\"\n  }\n}\n";
+var body2 = Encoding.UTF8.GetBytes(message2);
+
+channel2.BasicPublish(exchange: string.Empty,
+    routingKey: queueName,
+    basicProperties: null,
+    body: body2);
+Console.WriteLine($" [x] Sent {message2}");
+
+// Console.WriteLine(" Press [enter] to exit.");
+// Console.ReadLine();
---
+++ RabbitMqWaitRequestAndSendRespons/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+const string RequestQueueName = "temperatureRequestQueue"; 
+const string ResponseQueueName = "temperatureResponseQueue";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: RequestQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueDeclare(queue: ResponseQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine($" [*] Waiting for messages in {RequestQueueName}.");
+// Thread.Sleep(5000);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    // Thread.Sleep(5000);
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var correlationId = ea.BasicProperties.CorrelationId;
+    Console.WriteLine($" [x] Received message: {message} with CorrelationId: {correlationId}");
+
+    var random = new Random();
+    double temperature = random.NextDouble() * 10000;
+    double humidity = random.NextDouble() * 10000;
+
+    var responseMessage = new
+    {
+        temperature,
+        humidity
+    };
+    var jsonResponse = JsonConvert.SerializeObject(responseMessage, Formatting.Indented);
+    var responseBody = Encoding.UTF8.GetBytes(jsonResponse);
+
+    // Create properties for the response message
+    var replyProps = channel.CreateBasicProperties();
+    replyProps.CorrelationId = correlationId;
+    // Thread.Sleep(5000);
+    // Publish the response message
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: ResponseQueueName,
+                         basicProperties: replyProps,
+                         body: responseBody);
+    Console.WriteLine($" [x] Sent response: {responseMessage} with CorrelationId: {correlationId}");
+
+    // Manually acknowledge the message after processing
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+// Thread.Sleep(5000);
+channel.BasicConsume(queue: RequestQueueName, autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
